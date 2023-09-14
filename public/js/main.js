ccb161e58ef1b6ca9cbfe7086ddaa86548759ecb
Jonathan Cardona <jcardona0110@gmail.com>
2023-09-13 21:31:08 -0400

    finished styling mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,27 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const hidden = document.querySelector(".dateAndButton");
+const image = document.querySelector("img");
+const video = document.querySelector("iframe");
+
 document.querySelector("button").addEventListener("click", getFetch);
 
 function getFetch() {
 	const choice = document.querySelector("input").value;
 	console.log(choice);
 	const url = `https://api.nasa.gov/planetary/apod?api_key=3RshQFbXqVQAfd7Js9ffifDGnoGgb1OPG1uq5gGm&date=${choice}`;
-
 	fetch(url)
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data);
 			if (data.media_type === "image") {
-				document.querySelector("img").src = data.hdurl;
+				image.src = data.url; // Set img source to image URL
 				image.style.display = "block"; // Show img element
-				video.style.display = "none";
-			} else if (data.media_type === "video") {
-				document.querySelector("iframe").src = data.hdurl;
+				video.style.display = "none"; // Hide video element
+			} else {
+				// If media type is a video, show the video and hide the image
 				image.style.display = "none"; // Hide img element
-
-				video.style.display = "block";
+				video.src = data.url; // Set iframe source to video URL
+				video.style.display = "block"; // Show video element
 			}
 			document.querySelector("h3").innerText = data.explanation;
 		})
@@ -26,16 +29,9 @@ function getFetch() {
 			console.log(`error ${err}`);
 		});
 }
-const image = document.querySelector("img");
-const video = document.querySelector("iframe");
-// If media type is an image, show the image and hide the video
-if (data.media_type === "image") {
-	image.src = data.url; // Set img source to image URL
-	image.style.display = "block"; // Show img element
-	video.style.display = "none"; // Hide video element
-} else {
-	// If media type is a video, show the video and hide the image
-	image.style.display = "none"; // Hide img element
-	video.src = data.url; // Set iframe source to video URL
-	video.style.display = "block"; // Show video element
+// hide select date
+document.querySelector("button").addEventListener("click", hide);
+
+function hide() {
+	hidden.style.display = "none";
 }
